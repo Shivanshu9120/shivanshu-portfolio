25857ceae6a9d3a49104406cf0c232281f3f7341
Shivanshu Singh <kshatriyasarkar214@gmail.com>
2025-10-25 15:33:21 +0530

    Fixed build errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { Menu, X, User } from 'lucide-react'
+import { Menu, X } from 'lucide-react'
 import { motion } from 'framer-motion'
 import { useTheme } from '../context/ThemeContext'
 
